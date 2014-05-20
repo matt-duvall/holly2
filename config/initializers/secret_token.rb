c7100d16b85f203aa1e11435e951434ac8ac178f
matt-duvall <mduvall@bates.edu>
2014-05-20 08:28:07 -0400

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Holly2::Application.config.secret_key_base = '19649203bad76ca27e593b4229b12e4eedfc84574bd1e6e763c6934f1fc41664ad01a165ca4dd927513b191c7db6fd17eee9056a5feeb55b3460593e744710d4'
